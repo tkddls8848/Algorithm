93438d52169e2adfe1b376af02058128179ad83b
tkddls8848 <tkddls8848@gmail.com>
2019-10-05 10:29:04 +0900

    learn Permutaion Algorithm

---
+++ codingTest/Coding/src/com/kitri/code/Test.java
@@ -0,0 +1,11 @@
+package com.kitri.code;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str = "sadf";
+		System.out.println(str.substring(0, 1) + str.substring(2, str.length()));
+
+	}
+
+}
