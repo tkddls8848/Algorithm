223ca1599b867282070bb2de8e1e87c187deb4b0
tkddls8848 <tkddls8848@gmail.com>
2019-10-05 12:49:40 +0900

    Permutation 연습과 관련 메모

---
+++ codingTest/Coding/src/com/kitri/code/Permutation/Permutation.java
@@ -0,0 +1,62 @@
+package com.kitri.code.Permutation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Permutation {
+    
+    public static Boolean isPrime(int n){
+        
+    	boolean prime = false;
+    	
+        if(n == 1) {
+        	prime = false;
+        } else {
+            for(int a=2 ; a < Math.sqrt(n) ; a++){
+                if(n%a==0){
+                    prime=false;
+                    break;
+                } else if(n%a !=0) {
+                    prime=true;
+                }
+            }
+        }
+        System.out.println("isPrime"+prime);
+        return prime;
+    }
+    
+    public static void permutationNumber(String prefix, String str, HashSet<Integer> answerList) {
+    	
+    	int len = str.length();
+    	
+    	if(!prefix.equals("") && str.length() == 0) {
+    		answerList.add(Integer.parseInt(prefix));
+    	}
+
+    	for(int i = 0 ; i < len ; i ++) {
+    		permutationNumber(prefix + str.charAt(i), str.substring(0,i) + str.substring(i+1, len), answerList);
+    	}
+    	
+    }
+    
+	public static void main(String[] args) {
+		
+		String str = "112";
+		HashSet<Integer> answerList = new HashSet<Integer>();
+		int result = 0;
+		
+		permutationNumber("",str,answerList);
+		
+		while(answerList.iterator().hasNext()) {
+			int tmp = answerList.iterator().next();
+			answerList.remove(tmp);
+			
+			if(isPrime(tmp)) {
+				result++;
+			}
+		}
+		
+		System.out.println(result);
+	}
+    
+}
---
+++ codingTest/Coding/src/com/kitri/code/Permutation/Permutation2.java
@@ -0,0 +1,43 @@
+package com.kitri.code.Permutation;
+
+class Permutation2 {
+    
+	public static void permutation(int[] arr, int depth, int n, int r) {
+		
+		if(depth == r) {
+			print(arr, r);
+		}
+		
+		for(int i = depth ; i < n ; i++) {
+			swap(arr, i, depth);
+			permutation(arr, depth + 1, n, r);
+			swap(arr, i, depth);
+		}
+		
+	}
+	
+	public static void print(int[] arr, int r) {
+		for(int i = 0 ; i < arr.length ; i++) {
+			if(i != arr.length-1) {
+				System.out.print(arr[i]);				
+			} else {
+				System.out.println(arr[i]);					
+			}
+
+		}
+	}
+	
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+	
+	public static void main(String[] args) {
+
+		int[] arr = {1,2,3,4};
+		permutation(arr,0,4,4);
+		
+	}
+    
+}
