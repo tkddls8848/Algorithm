b65aee1be6aef595b8d10177ea47aff58febd82d
PSI <tkddls8848@naver.com>
2019-10-23 15:02:41 +0900

    2018 서머코딩 코딩테스트 - 블럭 쌓기(효율성 비통과)

---
+++ codingTest/Coding/src/com/kitri/WinterCoding/WinterCoding3.java
@@ -0,0 +1,64 @@
+package com.kitri.WinterCoding;
+
+import java.util.*;
+
+public class WinterCoding3 {
+
+	public static int destruction(int[][] land, int P, int height) {
+		int desCost = 0;
+
+		for(int i = 0 ; i < land.length ; i++) {
+			for(int j = 0 ; j < land[i].length ; j++) {
+				if(land[i][j]<=height) {
+					desCost += 0;
+				} else {
+					desCost += P*(land[i][j]-height);
+				}
+			}
+		}
+		return desCost;
+	}
+	
+	public static int construction(int[][] land, int Q, int height) {
+		int conCost = 0;
+
+		for(int i = 0 ; i < land.length ; i++) {
+			for(int j = 0 ; j < land[i].length ; j++) {
+				if(land[i][j]<=height) {
+					conCost += Q*(height-land[i][j]);
+				} else {
+					conCost += 0;
+				}
+			}
+		}
+		return conCost;
+	}
+    
+	
+	public static long solution(int[][] land, int P, int Q) {
+		long answer = -1;
+		int totalCost = 0;
+		int tmp = destruction(land,Q,0) + construction(land,P,0);
+		
+		for(int i = 1 ; i < Math.pow(10, 9) ; i++) {
+			totalCost = destruction(land,Q,i) + construction(land,P,i);
+			if(tmp < totalCost) {
+				answer = tmp;
+				break;
+			} else {
+				tmp = totalCost;
+			}
+		}
+
+		return answer;
+	}
+    
+    public static void main(String[] args) {
+
+    	int[][] land = {{4, 4, 3}, {3, 2, 2}, {2, 1, 0}};
+    	int P = 5;
+    	int Q = 3;
+    	
+    	solution(land, P, Q);
+    }
+}
--- codingTest/Coding/src/com/kitri/code/Solution.java
+++ codingTest/Coding/src/com/kitri/code/Solution.java
@@ -4,57 +4,39 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.Scanner;
 
-class Solution {
-	
-	public static ArrayList<Integer> judge(LinkedList<Integer> que, int time) {
-		
-		
-		Queue<Integer> queue = que;
-		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		int num = 0;
-		
-		while(!queue.isEmpty()) {
-			if(queue.poll() <= time) {
-				num++;
-			} else {
-				arrayList.add(num);
-				num = 1;
-				arrayList.add(num);
-			}
-		}
-
-		return arrayList;
-	}
-
-	public static ArrayList<Integer> complete(int[] p, int[] s, int time) {
-		
-		int duration=0;
-		LinkedList<Integer> ll = new LinkedList<>();
-		for(int i = 0 ; i < p.length ; i++) {
-			while(p[i]<100) {
-				p[i] += s[i];
-				duration++;
-			}
-			ll.add(i, duration);
-			System.out.println(duration);
-			duration=0;
-		}
-		return judge(ll, time);
-	}
+public class Solution {
+	static int Arr[];
+	static int Sum = 0;
+	static int Count = 0;
 
 	public static void main(String[] args) {
-
-		int[] progresses = { 93, 30, 55 };
-		int[] speeds = { 1, 30, 5 };
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		ArrayList<Integer> arrayList = complete(progresses, speeds, 7);	
-		
-		for(int i = 0 ; i < arrayList.size() ; i++) {
-			System.out.println(arrayList.get(i));
+		System.out.println("http://jsieun73.tistory.com/");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("몇개의 수와 원하는 합을 말하시오 ");
+		int _Size = scan.nextInt();
+		Sum = scan.nextInt();
+		Arr = new int[_Size];
+		System.out.println("수들을 입력하시오 ");
+		for (int i = 0; i < _Size; i++) {
+			Arr[i] = scan.nextInt();
 		}
+		partSum(_Size);
+		System.out.println("답은 : " + Count);
 
 	}
 
+	static void partSum(int _Size) {
+		int trySum = 0;
+		for (int i = 0; i < _Size; i++) {
+			trySum = 0;
+			for (int j = i; j < _Size; j++) {
+				trySum += Arr[j];
+				if (Sum == trySum) {
+					Count++;
+				}
+			}
+		}
+	}
 }
