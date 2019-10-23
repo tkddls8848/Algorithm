0f559183031ba2a70c0c2da9d787fe3b61920d62
PSI <tkddls8848@naver.com>
2019-10-23 15:05:19 +0900

    2018 서머코딩 코딩테스트 - 예산

---
+++ codingTest/Coding/src/com/kitri/WinterCoding/WinterCoding4.java
@@ -0,0 +1,31 @@
+package com.kitri.WinterCoding;
+
+import java.util.*;
+
+public class WinterCoding4 {
+
+	public static int solution(int[] d, int budget) {
+		int answer = 0;
+		int sum = 0;
+		
+		Arrays.sort(d);
+		for(int i = 0 ; i < d.length ; i++) {
+			sum += d[i];
+			if(sum > budget) {
+				break;
+			} else {
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+		int[] d = { 2,2,3,3 };
+		int budget = 10;
+
+		solution(d, budget);
+	}
+}
