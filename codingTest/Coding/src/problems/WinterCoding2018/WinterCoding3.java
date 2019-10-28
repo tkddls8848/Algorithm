3fa2cf45c87e3d2141d30ff219404c60872382b2
PSI <tkddls8848@naver.com>
2019-10-28 15:54:21 +0900

    2018 윈터코딩 코딩테스트 - 쿠키 구입

--- codingTest/Coding/src/problems/WinterCoding2018/WinterCoding3.java
+++ codingTest/Coding/src/problems/WinterCoding2018/WinterCoding3.java
@@ -4,45 +4,46 @@ import java.util.*;
 
 public class WinterCoding3 {
 
-	public static void combination(int[] cookie, int index, int n, int k, int flag, int[] answer) {
+	static int[] pSum;
 
-
-		if(k == 0) {
-			System.out.println(answer[0]+""+answer[1]+""+answer[2]);
-		} else if(index == n) {
-			return;
-		} else {
-			answer[flag] = index;
-			combination(cookie, index+1, n, k-1, flag+1, answer);
-			combination(cookie, index+1, n, k,  flag, answer);
-
+	public static int solution(int[] cookie) {
+		int answer = 0;
+		pSum = new int[cookie.length + 1];
+		for (int i = 1; i < pSum.length; i++) {
+			pSum[i] = pSum[i - 1] + cookie[i - 1];
 		}
-		
+		for (int i = 1; i < pSum.length - 1; i++) {
+			for (int j = pSum.length - 1; j > i; j--) {
+				int r = find(i, j);
+				if (answer < r) {
+					answer = r;
+				}
+			}
+		}
+		return answer;
 	}
-	
-    public static int solution(int[] cookie) {
-    	int result=0;
-        int[] answer = new int[6];
-//        	숫자 사이에 3개의 칸막이 세개의 인덱스 combination
-//        l~m m+1~r각각의 합
-//        같으면 목록에 추가
-//        각 단계 후보군 sort 후 가장 큰거 답
-        
-//		int n = cookie.length + 1;
-//		int k = 3;
-//		int[] arr = new int[n];
-//		for(int i = 0 ; i < n; i++) {
-//			arr[i] = i;
-//		}        
-        combination(cookie, 0, 5, 3, 0, answer);
-        return result;
-    }
-    
-    public static void main(String[] args) {
 
-    	int[] cookie = {1,2,3,4,8};
-    	
-    	solution(cookie);
-    	
-    }
+	public static int find(int start, int end) {
+		int i = start, j = end;
+		while (start < end) {
+			int mid = (start + end) / 2;
+			int s1 = pSum[mid] - pSum[i - 1], s2 = pSum[j] - pSum[mid];
+			if (s1 == s2) {
+				return s1;
+			} else if (s1 < s2) {
+				start = mid + 1;
+			} else {
+				end = mid;
+			}
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+		int[] cookie = { 1, 2, 3, 1 };
+
+		solution(cookie);
+
+	}
 }
